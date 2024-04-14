d0ec1978c88f0fa101352fb900b00e9d62c3f3ea
Jorge Fernandez <jfer4798@gmail.com>
2024-04-14 16:45:35 -0400

    Agregamos Acces-control-Allow

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
 import dotenv from 'dotenv';
 dotenv.config()
-import express  from 'express';
+import express from 'express';
 import authRoutes from './routes/authRoutes'
 import usersRoutes from './routes/userRoutes'
 
 const app = express()
 app.use(express.json())
 
+// Middleware para permitir solicitudes CORS desde cualquier origen
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Configura para permitir solicitudes desde cualquier origen
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Configura los métodos permitidos
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Configura los encabezados permitidos
+    next();
+});
+
 //Routes
-app.use('/auth',authRoutes)
+app.use('/auth', authRoutes)
 //autenticacion
-app.use('/users',usersRoutes)
+app.use('/users', usersRoutes)
 //user
 
 
